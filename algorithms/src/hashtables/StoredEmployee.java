044f1cced27bd55edbacdcdec5df84716491798e
Gautham D N <gauthamdn@gmail.com>
2024-11-03 16:59:57 +0530

    Hashtables - Linear Probing
    Hashtables - Linear Probing

---
+++ algorithms/src/hashtables/StoredEmployee.java
@@ -0,0 +1,13 @@
+package hashtables;
+
+public class StoredEmployee {
+
+	public String key;
+	public Employee employee;
+	
+	public StoredEmployee(String key, Employee employee) {
+		this.key = key;
+		this.employee = employee;
+	}
+	
+}
