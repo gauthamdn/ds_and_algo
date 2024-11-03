6c7801516d3e1c4fbbd2cb0eb3fe39f441b85b32
Gautham D N <gauthamdn@gmail.com>
2024-11-03 16:30:56 +0530

    Hastable - simple hash table using Array
    Hastable - simple hash table using Array

---
+++ algorithms/src/hashtables/SimpleHashTable.java
@@ -0,0 +1,38 @@
+package hashtables;
+
+public class SimpleHashTable {
+	
+	private Employee[] hashtable;
+
+	public SimpleHashTable() {
+		this.hashtable = new Employee[10];
+	}
+	
+	
+	private int hashKey(String key) {
+		
+		return key.length() % hashtable.length;
+	}
+	
+	
+	public void put(String key, Employee employee) {
+		int hashedKey = hashKey(key);
+		if(hashtable[hashedKey]!=null)
+			System.out.println("Sorry, there is already an employee at this position"+ hashedKey);
+		else
+			hashtable[hashedKey] = employee;
+	}
+	
+	public Employee get(String key) {
+		int hashedKey = hashKey(key);	
+		return hashtable[hashedKey];
+	}
+	
+	
+	public void printHashTable() {
+		for(int i = 0; i<hashtable.length;i++) {
+			System.out.println(hashtable[i]);
+		}
+	}
+
+}
